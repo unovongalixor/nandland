5945380400af9b453ed74e4f826818ec7ac07ad4
Simon Bursten <sbursten@augury.com>
2024-02-24 14:11:09 +0200

    added test for toggle module

--- 4_switch_debounce/tests/test_led_toggle.py
+++ 4_switch_debounce/tests/test_led_toggle.py
@@ -1,25 +1,41 @@
 import cocotb
-from cocotb.triggers import FallingEdge, Timer
+from cocotb.triggers import FallingEdge, RisingEdge, Timer
 
 
 async def generate_clock(dut):
     """Generate clock pulses."""
 
-    for cycle in range(10):
+    while(True):
         dut.i_Clk.value = 0
         await Timer(1, units="ns")
         dut.i_Clk.value = 1
         await Timer(1, units="ns")
 
+async def toggle_switch(dut):
+    await RisingEdge(dut.led_toggle_inst.i_Clk)  
+    dut.led_toggle_inst.i_Switch_1.value = 1;
+    await RisingEdge(dut.led_toggle_inst.i_Clk) 
+    dut.led_toggle_inst.i_Switch_1.value = 0;
+    await RisingEdge(dut.led_toggle_inst.i_Clk)
+    await RisingEdge(dut.led_toggle_inst.i_Clk)
 
 @cocotb.test()
-async def my_second_test(dut):
-    """Try accessing the design."""
-
+async def toggle_test(dut):
     await cocotb.start(generate_clock(dut))  # run the clock "in the background"
 
-    await Timer(5, units="ns")  # wait a bit
-    await FallingEdge(dut.i_Clk)  # wait for falling edge/"negedge"
+    await RisingEdge(dut.led_toggle_inst.i_Clk)
 
-    dut._log.info("o_LED_1 is %s", dut.led_toggle_inst.o_LED_1.value)
+    # led starts off
+    assert dut.led_toggle_inst.o_LED_1.value[0] == 0, "o_LED_1[0] is not 0!"
+
+    # toggle switch
+    await toggle_switch(dut)
+
+    # led should be on now 
+    assert dut.led_toggle_inst.o_LED_1.value[0] == 1, "o_LED_1[0] is not 1!"
+
+    # toggle switch
+    await toggle_switch(dut)
+
+    # led should be off now 
     assert dut.led_toggle_inst.o_LED_1.value[0] == 0, "o_LED_1[0] is not 0!"
