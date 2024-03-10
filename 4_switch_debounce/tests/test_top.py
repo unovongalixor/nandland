c766ecef757fd83ec6425abe5289125b9d64cbd4
Simon Bursten <sbursten@augury.com>
2024-03-10 15:18:14 +0200

    added top level test, still haven't added reset functions (would simplify resetting state between tests)

--- 4_switch_debounce/tests/test_top.py
+++ 4_switch_debounce/tests/test_top.py
@@ -11,46 +11,42 @@ async def generate_clock(dut):
         dut.i_Clk.value = 1
         await Timer(1, units="ns")
 
-
 @cocotb.test()
-async def debounce_test(dut):
+async def top_test(dut):
     """
     test the output filter
 
     note that DEBOUNCE_LIMIT is set to 30 in tests/Makefile
     """
     await cocotb.start(generate_clock(dut))  # run the clock "in the background"
+    await Timer(5, units="ns")
     await FallingEdge(dut.i_Clk)  # wait for falling edge/"negedge"
 
-    dut.debounce_filter_inst.i_Bouncy.value = 0
+    # toggle input
+    dut.i_Switch_1.value = 0
+
     await FallingEdge(dut.i_Clk)
     await FallingEdge(dut.i_Clk)
-
-    await Timer(135, units="ns")
-
-    # debounce filter output starts low
-    assert dut.debounce_filter_inst.o_Debounced.value[0] == 0, "o_Debounced is not 0!"
+    assert dut.i_Switch_1.value[0] == 0, "i_Switch_1 is not 0!"
 
     # toggle input
-    dut.debounce_filter_inst.i_Bouncy.value = 1
+    dut.i_Switch_1.value = 1
 
     await FallingEdge(dut.i_Clk)
-    assert dut.debounce_filter_inst.i_Bouncy.value[0] == 1, "i_Bouncy is not 1!"
+    await FallingEdge(dut.i_Clk)
+    assert dut.i_Switch_1.value[0] == 1, "i_Switch_1 is not 1!"
 
     # validate that output isn't set (enough time hasn't elapsed)
-    assert dut.debounce_filter_inst.o_Debounced.value[0] == 0, "o_Debounced is not 0!"
+    assert dut.o_LED_1.value[0] == 0, "o_LED_1 is not 0!"
 
     # wait a bit (not enough to trigger the led)
     await Timer(25, units="ns")
 
-    count = int(dut.debounce_filter_inst.r_Count.value)
-
     # validate that led isn't set (enough time hasn't elapsed)
-    assert dut.debounce_filter_inst.o_Debounced.value[0] == 0, "o_Debounced is not 0!"
+    assert dut.o_LED_1.value[0] == 0, "o_LED_1 is not 0!"
 
     # wait more than DEBOUNCE_LIMIT
     await Timer(50, units="ns")
 
     # validate that output is high
-    assert dut.debounce_filter_inst.o_Debounced.value[0] == 1, "o_Debounced is not 1!"
-
+    assert dut.w_Debounced_Switch.value[0] == 1, "o_LED_1 is not 1!"
